a9e9e8798c983c99a2831ec7937f947819cb1c7f
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-21 22:48:32 +0200

    Added a js file in ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function compareDifferentValues(m, n){
+    if(m===n){
+        return "Equal";
+    }
+    return "Not equal";
+}
+compareDifferentValues(3,"3");
+// Only change code above this line
+module.exports = compareDifferentValues;
