56d7fde81a7c3e284a4cc1682324c0723ca2a08b
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-21 23:02:53 +0200

    Added a js file in ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function compareStrictValues(m){
+    if(m!=55){
+        return "Not equal";
+    }
+    return "Equal";
+}
+compareStrictValues(54);
+// Only change code above this line
+module.exports = compareStrictValues;
