c4bc9f2995bdef8506a031721e6428056f7981fa
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-21 22:24:23 +0200

    Added a js file in ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function myFunction(myParameter){
+    if(myParameter){
+        return "The parameter is true!";
+    }
+    return "The parameter is false!";
+}
+myFunction(true);
+// Only change code above this line
+module.exports = myFunction;
