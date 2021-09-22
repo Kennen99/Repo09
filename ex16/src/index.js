375ea3f4669cea752eea49a02a0303123db35654
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-22 10:01:33 +0200

    Added a js file in ex16

---
+++ ex16/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function checkSign(num){
+    return (num>0) ? "positive"
+        : (num<0) ? "negative"
+        : "zero";
+}
+checkSign(0);
+// Only change code above this line
+module.exports = checkSign;
