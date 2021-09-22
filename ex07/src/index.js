d3006c3b8e7d48ed311340895a121d2eb14a3acb
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-22 08:50:29 +0200

    Added a js file in ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function logicalAndOperator(num){
+    if(num>=48 && num<=80){
+        return "Yes";
+    }
+    return "No";
+}
+console.log(logicalAndOperator(80));
+// Only change code above this line
+module.exports = logicalAndOperator;
